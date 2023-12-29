ec9b4cd76cf560f50e631f39f825778102bee786
smhasanjamil <smhasanjamil23@gmail.com>
2023-12-30 00:26:19 +0600

    Services page content added

--- src/pages/Doctors/Doctors.jsx
+++
@@ -1,9 +0,0 @@
-const Doctors = () => {
-  return (
-    <>
-      <h3>Doctors</h3>
-    </>
-  );
-};
-
-export default Doctors;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,17 @@
+import { services } from "../assets/data/services";
+import ServiceCard from "../components/Services/ServiceCard";
+
 const Services = () => {
   return (
-    <>
-      <h3>Services</h3>
-    </>
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px]">
+          {services?.map((item, index) => (
+            <ServiceCard item={item} index={index} key={index} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
