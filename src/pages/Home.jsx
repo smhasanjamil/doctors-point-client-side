822e013a05e1e7ab2b99a20fe0920bc155ad2123
smhasanjamil <smhasanjamil23@gmail.com>
2023-12-29 15:20:56 +0600

    faq section added to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import heroImg03 from "../assets/images/hero-img03.png";
 import featureImg from "../assets/images/feature-img.png";
 import videoImg from "../assets/images/video-icon.png";
 import avatorIcon from "../assets/images/avatar-icon.png";
+import faqImg from "../assets/images/faq-img.png";
 
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
@@ -14,6 +15,7 @@ import icon03 from "../assets/images/icon03.png";
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorList from "../components/Doctors/DoctorList";
+import FaqList from "../components/Faq/FaqList";
 
 const Home = () => {
   return (
@@ -259,6 +261,25 @@ const Home = () => {
         </div>
       </section>
       {/* our great doctro section end ======= */}
+
+      {/* faq section start ======= */}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="faqImg" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* faq section end ======= */}
     </>
   );
 };
